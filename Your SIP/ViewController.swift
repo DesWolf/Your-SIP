9f052ee9cf40bc62229c5a52849f2b9678b2e324
Max Okuneev <okuneev@gmail.com>
2020-01-16 20:18:28 +0700

    fix bugs, add possibility to change number directly

--- Your SIP/ViewController.swift
+++ Your SIP/ViewController.swift
@@ -10,23 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var ageLabel: UILabel!
-    @IBOutlet var retirementAgeLabel: UILabel!
-    @IBOutlet var sumLabel: UILabel!
-    @IBOutlet var persentLabel: UILabel!
+    @IBOutlet var ageLabel: UITextField!
+    @IBOutlet var retirementAgeLabel: UITextField!
+    @IBOutlet var sumLabel: UITextField!
+    @IBOutlet var percentLabel: UITextField!
     @IBOutlet var resultLabel: UILabel!
     @IBOutlet var calculateButton: UIButton!
     
+   
+    @IBOutlet var gestureReconizer: UILongPressGestureRecognizer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         sumLabel.textAlignment = .natural
         calculateButton.layer.cornerRadius = calculateButton.frame.size.height / 2
+        
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneAction))
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        toolbar.setItems([flexSpace, doneButton], animated: true)
+        
+        ageLabel.inputAccessoryView = toolbar
+        retirementAgeLabel.inputAccessoryView = toolbar
+        sumLabel.inputAccessoryView = toolbar
+        percentLabel.inputAccessoryView = toolbar
+      
     }
     
     var age = 27
     var retirementAge = 45
     var sum = 100
-    var persent = 5
+    var percent = 5
     var result = 0
     
     override var prefersStatusBarHidden: Bool {
@@ -34,6 +51,7 @@ class ViewController: UIViewController {
     }
     @IBAction func agePlus(_ sender: Any) {
         
+        age = Int(ageLabel.text!) ?? 27
         if age < 100 {
             if retirementAge - age == 1 {
                 retirementAge = retirementAge + 1
@@ -48,18 +66,21 @@ class ViewController: UIViewController {
     
     @IBAction func ageMinus(_ sender: Any) {
         
+        age = Int(ageLabel.text!) ?? 27
         if age > 5 { age = age - 1 }
         ageLabel.text = String(age)
     }
     
     @IBAction func retirementAgePlus(_ sender: Any) {
         
+        retirementAge = Int(retirementAgeLabel.text!) ?? 45
         if retirementAge < 100 { retirementAge = retirementAge + 1 }
         retirementAgeLabel.text = String(retirementAge)
     }
     
     @IBAction func retirementAgeMinus(_ sender: Any) {
     
+        retirementAge = Int(retirementAgeLabel.text!) ?? 45
         if retirementAge > 30 {
             if retirementAge - age == 1 {
                 age = age - 1
@@ -74,30 +95,54 @@ class ViewController: UIViewController {
     
     @IBAction func sumPlus(_ sender: Any) {
         
+        sum = Int(sumLabel.text!) ?? 100
+        if sumLabel.text!.last == "$" {
+            let sumString = sumLabel.text!.dropLast()
+        sum = Int(sumString)!
+        }
         if  sum < 98000 { sum = sum + 100 }
-        sumLabel.text = String(sum) 
+        sumLabel.text = "\(sum)$"
     }
 
     @IBAction func sumMinus(_ sender: Any) {
         
+        sum = Int(sumLabel.text!) ?? 100
+        if sumLabel.text!.last == "$" {
+                 let sumString = sumLabel.text!.dropLast()
+             sum = Int(sumString)!
+             }
         if  sum > 100 { sum = sum - 100 }
-        sumLabel.text = String(sum)
+        sumLabel.text = "\(sum)$"
     }
     
     @IBAction func persentPlus(_ sender: Any) {
         
-        if persent < 30 { persent = persent + 1 }
-        persentLabel.text = "\(persent)%"
+        percent = Int(percentLabel.text!) ?? 5
+        if percentLabel.text!.last == "%" {
+           let percentString = percentLabel.text!.dropLast()
+            percent = Int(percentString)!
+        }
+        if percent < 30 { percent = percent + 1 }
+        percentLabel.text = "\(percent)%"
         
     }
     @IBAction func percentMinus(_ sender: Any) {
         
-        if  persent > -5 { persent = persent - 1 }
-        persentLabel.text = "\(persent)%"
+        percent = Int(percentLabel.text!) ?? 5
+        if percentLabel.text!.last == "%" {
+           let percentString = percentLabel.text!.dropLast()
+            percent = Int(percentString)!
+        }
+        if  percent > -5 { percent = percent - 1 }
+        percentLabel.text = "\(percent)%"
     }
     
     @IBAction func calculate(_ sender: Any) {
 
+        let age = standartCheckForLettersAndNil(str: ageLabel.text!)
+        let retirementAge = standartCheckForLettersAndNil(str: retirementAgeLabel.text!)
+        let sum = standartCheckForLettersAndNil(str: sumLabel.text!)
+        let persent = standartCheckForLettersAndNil(str: percentLabel.text!)
         var i = 0
         let realPersent = (pow(1 + (Double(persent) / 100.0), 1.0/12.0) - 1) * 100
         var result = 0.0
@@ -114,7 +159,76 @@ class ViewController: UIViewController {
             formatter.groupingSeparator = " "
         return formatter.string(from: itIsANumber)!
         }
-        resultLabel.text = separatedNumber(Int(result))
+        resultLabel.text = "\(separatedNumber(Int(result))) $"
+    }
+   
+    private func alertWrongData() {
+        let alert = UIAlertController(title: "Ошибка!",
+                                      message: "Невозможно рассчитать. Пожалуйста, измените значение.",
+                                      preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default,handler: { _ in }))
+        self.present(alert, animated: true, completion: nil)
+    }
+}
+// MARK: Extension
+extension ViewController: UITextFieldDelegate {
+
+
+
+    @objc func textFieldDidChange(textField: UITextField){
+    }
+
+    @objc func doneAction(){
+        
+        var age = 0
+        var retirementAge = 0
+        var sum = 0
+        var percent = 0
+        
+    //age
+        age = standartCheckForLettersAndNil(str: ageLabel.text!)
+        if (age < 5 || age > 100) {
+            alertWrongData()
+        }
+        ageLabel.text = ("\(age)")
+        
+    //retirementAge
+        retirementAge = standartCheckForLettersAndNil(str: retirementAgeLabel.text!)
+        if (retirementAge < 20 || retirementAge > 115){
+            if age > retirementAge {
+            alertWrongData()
+            }
+        }
+        retirementAgeLabel.text = ("\(retirementAge)")
+        
+    //sumLabel
+        sum = standartCheckForLettersAndNil(str: sumLabel.text!)
+        if (sum < 1 || sum > 99000) {
+            alertWrongData()
+        }
+        sumLabel.text = ("\(sum)$")
+        
+    //percentLabel
+        percent = standartCheckForLettersAndNil(str: percentLabel.text!)
+        if (percent < 1 || percent > 100) {
+        alertWrongData()
+        }
+        percentLabel.text = ("\(percent)%")
+        
+        view.endEditing(true)
+        
+    }
+
+    func standartCheckForLettersAndNil (str:String) -> (Int){
+      
+        if str == "" {
+            return 0
+        } else if (str.last == "%" || str.last == "$"){
+            return  (Int(str.dropLast())!)
+        }
+        else {
+            return (Int(str)!)
+        }
     }
 }
 
